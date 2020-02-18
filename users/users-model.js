531a8756a31dda803dcc83a94260c8e592012ea9
Danica Valdez <danicaevaldez@gmail.com>
2020-02-17 20:47:42 -0900

    users-model set up

---
+++ users/users-model.js
@@ -0,0 +1,27 @@
+const db = require("../database/dbConfig");
+
+module.exports = {
+  find,
+  findBy,
+  findById,
+  add
+};
+
+function find() {
+  return db("users");
+}
+
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first();
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+async function add(user) {
+  const [id] = await db("users").insert(user);
+  return findById(id);
+}
